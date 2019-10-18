72b84b11d1730b7c00620c0cebd803681acaf1e8
sahil-athrij <sahilathrij@gmail.com>
2019-10-18 06:41:43 +0530

    fixed errors

---
+++ app.py
@@ -0,0 +1,22 @@
+from model.network import Net
+from flask import Flask, jsonify, render_template, request
+from preprocessing import *
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/digit_prediction', methods=['POST'])
+
+def digit_prediction():
+    if(request.method == "POST"):
+        img = request.get_json()
+        img = preprocess(img)
+        net = Net()
+        digit, probability = net.predict_with_pretrained_weights(img, 'pretrained_weights.pkl')
+        data = { "digit":int(digit), "probability":float(int(probability*100))/100}
+        print(data)
+        return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
